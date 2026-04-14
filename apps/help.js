a9b0092051cfdd6b7c6194eb5f781b67fe15fafb
xiaoqianghepipi <wqwqwqwqds@outlook.com>
2026-04-14 22:42:51 +0800

    copliot尝试修复渲染问题x4

--- apps/help.js
+++ apps/help.js
@@ -22,7 +22,7 @@ export class Help extends plugin {
     try {
       logger.info('[归龙潮插件] 正在动态生成帮助图...')
 
-      const img = await puppeteer.render('help', buildHelpData(), {
+      const img = await puppeteer.render('help/index', buildHelpData(), {
         e,
         scale: 1.2,
         retType: 'base64'
